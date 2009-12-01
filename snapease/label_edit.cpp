6a7f8cd604f5a8cbe444d0fcdabe7d390321bfb0
Cockos Inc <nospam@cockos.com>
2009-12-01 01:51:10 +0000

    LPGLification of snapease -- from d2dad223

--- snapease/label_edit.cpp
+++ snapease/label_edit.cpp
@@ -1,3 +1,23 @@
+/*
+    SnapEase
+    label_edit.cpp -- code for managing renaming of images
+    Copyright (C) 2009  Cockos Incorporated
+
+    PathSync is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    PathSync is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PathSync; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #include "main.h"
 
 #ifndef _WIN32
